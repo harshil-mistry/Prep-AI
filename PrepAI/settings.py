77ad378f464468e84cf42ac7e517f2a85cdedaf2
harshil-mistry <harshilmistry31@gmail.com>
2025-06-05 15:51:35 +0530

    Requirements file added

--- PrepAI/settings.py
+++ PrepAI/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5-%_u8+c6_eh+zs%rj9)4rs*ie$a90v+pn=qqz^l_p^a#y5v8w'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
